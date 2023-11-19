96ebe0144ac590ebada65f7c130112be417ded2e
Dylan Palmer <djpalmer321@gmail.com>
2023-11-19 17:24:34 -0500

    Changed Names

--- Assets/Scripts/RunnerAI.cs
+++ Assets/Scripts/RunnerAI.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class EnemyAi : MonoBehaviour
+public class RunnerAI : MonoBehaviour
 {
 
     public Transform Enemy;
