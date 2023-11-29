b4cc5ef4a482d87eb353f49b2b4394bb9a562488
Jason Griller <ja194255@ucf.edu>
2023-11-29 00:14:54 -0500

    Adding Cutscenes

--- Assets/KITO_DOSSOLVE_2/Scripts/Dissolve.cs
+++ Assets/KITO_DOSSOLVE_2/Scripts/Dissolve.cs
@@ -3,49 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Keto
+public class Dissolve : MonoBehaviour
 {
-    public class Dissolve : MonoBehaviour
-    {
-        public float DissolveSpeed = 0.01f;
-        public float DissolveYield = 0.1f;
+    public float DissolveSpeed = 0.01f;
+    public float DissolveYield = 0.1f;
 
-        public ParticleSystem Particle = null;
+    public ParticleSystem Particle = null;
         
 
-        protected const string DISSOVE_AMOUNT = "_DissolveAmount";
-        protected const string IDLE_TRIGGER = "Idle";
-        protected const string DISSOLVE_TRIGGER = "Death";
+    protected const string DISSOVE_AMOUNT = "_DissolveAmount";
+    protected const string IDLE_TRIGGER = "Idle";
+    protected const string DISSOLVE_TRIGGER = "Death";
 
-        protected SkinnedMeshRenderer[] m_skinnedMeshRenderers = null;
-        protected List<Material> m_materials = new List<Material>();
-        protected Animator m_animationController = null;
+    protected SkinnedMeshRenderer[] m_skinnedMeshRenderers = null;
+    protected List<Material> m_materials = new List<Material>();
+    protected Animator m_animationController = null;
 
-        protected float m_dissolveStart = -0.2f;
-        protected float m_dissolveEnd = 1.2f;
+    protected float m_dissolveStart = -0.2f;
+    protected float m_dissolveEnd = 1.2f;
 
-        private void Awake()
+    private void Awake()
+    {
+        m_animationController = this.GetComponent<Animator>();
+        m_skinnedMeshRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>();
+        for (int i = 0; i < m_skinnedMeshRenderers.Length; i++)
         {
-            m_animationController = this.GetComponent<Animator>();
-            m_skinnedMeshRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>();
-            for (int i = 0; i < m_skinnedMeshRenderers.Length; i++)
+            for (int j = 0; j < m_skinnedMeshRenderers.Length; j++)
             {
-                for (int j = 0; j < m_skinnedMeshRenderers.Length; j++)
-                {
-                    m_materials.Add(m_skinnedMeshRenderers[j].material);
-                }
+                m_materials.Add(m_skinnedMeshRenderers[j].material);
             }
         }
+    }
 
-        public void StartDissolve()
-        {
-            StopAllCoroutines();
-            StartCoroutine(DissolveCoroutine());
-        }
+    public void StartDissolve()
+    {
+        StopAllCoroutines();
+        StartCoroutine(DissolveCoroutine());
+    }
 
-        protected virtual IEnumerator DissolveCoroutine()
-        {
-            yield return null;
-        }
+    protected virtual IEnumerator DissolveCoroutine()
+    {
+        yield return null;
     }
 }
